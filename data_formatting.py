2b9cbc2983fc64d2b925aee02a2b90c7ad8d8c7c
Emil Carlé Dietrich <emil.c.dietrich@gmail.com>
2022-02-19 13:56:17 +0100

    Finished data_formatting.py
    And exported the resulting street_trees.gpkg

--- data_formatting.py
+++ data_formatting.py
@@ -1,35 +1,143 @@
 #%% IMPORTS
 import geopandas as gp
+from numpy import row_stack
 
 #%% DATA
 copenhagenTrees = gp.read_file('data_raw/copenhagen_trees.gpkg')
-frederiksbergTrees = gp.read_file('data_raw/frederiksberg_trees.gpkg')
+
+frederiksbergStreetTrees = gp.read_file('data_raw/frederiksberg_trees.gpkg')
 
 #%% CLEANING DATAFRAMES
-copenhagenTrees["municipality"] = 'copenhagen'
-copenhagenTrees = copenhagenTrees.filter(['kategori', 'municipality', 'traeart', 'planteaar', 'busk_trae', 'vejnavn', 'bydelsnavn', 'stammeomfang', 'omgivelse', 'geometry'])
-copenhagenTrees.rename(columns = {
-    'kategori':'category',
+copenhagenStreetTrees = copenhagenTrees.loc[
+    (copenhagenTrees['kategori'] == 'gadetræ') & (copenhagenTrees['busk_trae'] == 'Træ')].copy(deep=True)
+
+copenhagenStreetTrees["municipality"] = 'copenhagen'
+
+copenhagenStreetTrees = copenhagenStreetTrees.filter(['municipality', 'traeart', 'planteaar', 'vejnavn', 'bydelsnavn', 'stammeomfang', 'omgivelse', 'geometry'])
+
+copenhagenStreetTrees.rename(columns = {
     'traeart':'species',
     'planteaar':'year',
-    'busk_trae':'bush_tree',
     'vejnavn':'street',
     'bydelsnavn':'neighbourhood',
     'stammeomfang':'trunk_diameter',
     'omgivelse':'surroundings'}, 
     inplace=True)
 
-frederiksbergTrees["municipality"] = 'frederiksberg'
-frederiksbergTrees = frederiksbergTrees.filter(['art_sort', 'municipality', 'vejnavn', 'geometry'])
-frederiksbergTrees.rename(columns = {
+frederiksbergStreetTrees["municipality"] = 'frederiksberg'
+
+frederiksbergStreetTrees = frederiksbergStreetTrees.filter(['art_sort', 'municipality', 'vejnavn', 'geometry'])
+
+frederiksbergStreetTrees.rename(columns = {
     'art_sort':'species',
     'vejnavn':'street'}, 
     inplace = True)
 
-#%% MERGING GEODATAFRAMES AND STANDERDISING 'species' column
-combinedTrees = copenhagenTrees.append(frederiksbergTrees)
+#%% MERGING GEODATAFRAMES
+streetTrees = copenhagenStreetTrees.append(frederiksbergStreetTrees)
 
+#%% STANDARDISING/RENAMING IN 'species' COLUMN
+for i, row in streetTrees.iterrows():
+    if 'Tilia' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Tilia'
+    elif 'Acer' in str(row['species']) or 'Ácer' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Acer'
+    elif 'Alnus' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Alnus'
+    elif 'Betula' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Betula'
+    elif 'Robinia' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Robinia'
+    elif 'Ulmus' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Ulmus'
+    elif 'Sorbus' in str(row['species']) or 'Sórbus' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Sorbus'
+    elif 'Fraxinus' in str(row['species']) or 'Fráxinus' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Fraxinus'
+    elif 'Malus' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Malus'
+    elif 'Quercus' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Quercus'
+    elif 'Carpinus' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Carpinus'
+    elif 'Pinus' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Pinus'
+    elif 'Platanus' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Platanus'
+    elif 'Crataegus' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Crataegus'
+    elif 'Pyrus' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Pyrus'
+    elif 'Prunus' in str(row['species']) or 'Prúnus' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Prunus'
+    elif 'Corylus' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Corylus'
+    elif 'Larix' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Larix'
+    elif 'Aesculus' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Aesculus'
+    elif 'Populus' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Populus'
+    elif 'Fagus' in str(row['species']) or 'Fágus' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Fagus'
+    elif 'Thuja' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Thuja'
+    elif 'Ginkgo' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Ginkgo'
+    elif 'Taxus' in str(row['species']) or 'Táxus' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Taxus'
+    elif 'Picea' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Picea'
+    elif 'Juniperus' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Juniperus'
+    elif 'Castanea' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Castanea'
+    elif 'Tsuga' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Tsuga'
+    elif 'Ilex' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Ilex'
+    elif 'Ailanthus' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Ailanthus'
+    elif 'Gleditsia' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Gleditsia'
+    elif 'Laburnum' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Laburnum'
+    elif 'Metasequoia' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Metasequoia'
+    elif 'Liquidambar' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Liquidambar'
+    elif 'Styphnolobium' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Styphnolobium'
+    elif 'Liriodendron' in str(row['species']) or 'Liriodéndron' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Liriodendron'
+    elif 'Paulównia' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Paulownia'
+    elif 'Koelreuteria' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Koelreuteria'
+    elif 'Cedrús' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Cedrus'
+    elif 'Catálpa' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Catalpa'
+    elif 'Eleagnus' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Eleagnus'
+    elif 'Taxodium' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Taxodium'
+    elif 'Nothofágus' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Nothofagus'
+    elif 'Cercidiphýllum' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Cercidiphyllum'
+    elif 'Salix' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Salix'
+    elif 'Juglans' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Juglans'
+    elif 'Pterocárya' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Pterocarya'
+    elif 'Crataegus' in str(row['species']) or 'Crateagus' in str(row['species']) or 'crataegus' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'Crataegus'
+    elif 'Ikke registreret' in str(row['species']):
+        streetTrees.at[i, 'species'] = 'not_registered'
+    
+print(streetTrees['species'].value_counts())
 
-
-#%% R
-copenhagenTrees.drop(columns = ['id', 'saerligt_trae', 'type', ]
+#%% Export to file
+streetTrees.to_file('data_created/street_trees.gpkg')
