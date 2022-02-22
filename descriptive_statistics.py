94f84eae1448ca76461d4844bc199c1ae874099f
Emil Carlé Dietrich <emil.c.dietrich@gmail.com>
2022-02-22 16:58:25 +0100

    more data work

---
+++ descriptive_statistics.py
@@ -0,0 +1,44 @@
+#%% IMPORTS
+import geopandas as gp
+import plotly.express as px
+import plotly.figure_factory as ff
+
+#%% DATA
+streetTrees = gp.read_file('data_created/street_trees.gpkg')
+
+#%% CONSTRUCTION OF ADDITIONAL COLUMNS
+#calculate frequency of genus
+streetTrees['freq_genus'] = streetTrees.groupby('species')['species'].transform('count')
+
+#renaming species summing those with small frequencies
+streetTrees['species_large'] = streetTrees['species']
+streetTrees.loc[streetTrees['freq_genus'] < 500, 'species_large'] = 'Other species'
+streetTrees.loc[streetTrees['freq_genus'] < 100, 'species_large'] = 'Insignificant species'
+
+#generating column for later use in frequency graphs
+streetTrees['single_tree'] = 1
+
+#%% PIE CHART: GENUS FREQUENCY
+pieGenusFreq = px.pie(streetTrees, 
+    values = 'single_tree', 
+    names = 'species_large', 
+    title = 'Distribution of Tree Genus')
+
+pieGenusFreq.show()
+
+#%% PIE CHART: FREQUENCY OF 'OTHER SPECIES' TREE GENUS
+streetTreesOtherSpecies = streetTrees.loc[streetTrees['species_large'] == 'Other species']
+
+pieOtherGenusFreq = px.pie(streetTreesOtherSpecies, 
+    values = 'single_tree', 
+    names = 'species', 
+    title = '''Distribution of Tree Genus' within "Other Genus" category''')
+
+pieOtherGenusFreq.show()
+#%% 
+
+fig = px.bar(streetTrees, x = 'species_large', y = 'single_tree', color = 'municipality')
+fig.show()
+
+
+# %%
