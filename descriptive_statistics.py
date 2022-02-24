d8f167cedb4b8d7e14f66153eb8b984bf859f8f8
Phil-Geo <philipliehr@googlemail.com>
2022-02-24 14:16:37 +0100

    Revert "Merge branch 'main' of https://github.com/emilcarle/urban_sustainability_exam"
    This reverts commit cf6c2b99e54b2b95de9118cebde99e2cc9bd1b25, reversing
    changes made to 2b846f22766c4fb9d94319e759bd414ea63114b0.

--- descriptive_statistics.py
+++ descriptive_statistics.py
@@ -1,18 +1,19 @@
 #%% IMPORTS
 import geopandas as gp
 import plotly.express as px
+import plotly.figure_factory as ff
 
 #%% DATA
 streetTrees = gp.read_file('data_created/street_trees.gpkg')
 
 #%% CONSTRUCTION OF ADDITIONAL COLUMNS
 #calculate frequency of genus
-streetTrees['freq_genus'] = streetTrees.groupby('genus')['genus'].transform('count')
+streetTrees['freq_genus'] = streetTrees.groupby('species')['species'].transform('count')
 
-#renaming genus summing those with small frequencies
-streetTrees['genus_large'] = streetTrees['genus']
-streetTrees.loc[streetTrees['freq_genus'] < 500, 'genus_large'] = 'Other genus'
-streetTrees.loc[streetTrees['freq_genus'] < 100, 'genus_large'] = 'Insignificant genus'
+#renaming species summing those with small frequencies
+streetTrees['species_large'] = streetTrees['species']
+streetTrees.loc[streetTrees['freq_genus'] < 500, 'species_large'] = 'Other species'
+streetTrees.loc[streetTrees['freq_genus'] < 100, 'species_large'] = 'Insignificant species'
 
 #generating column for later use in frequency graphs
 streetTrees['single_tree'] = 1
@@ -20,27 +21,24 @@ streetTrees['single_tree'] = 1
 #%% PIE CHART: GENUS FREQUENCY
 pieGenusFreq = px.pie(streetTrees, 
     values = 'single_tree', 
-    names = 'genus_large', 
+    names = 'species_large', 
     title = 'Distribution of Tree Genus')
 
 pieGenusFreq.show()
 
-#%% PIE CHART: FREQUENCY OF 'OTHER genus' TREE GENUS
-streetTreesOthergenus = streetTrees.loc[streetTrees['genus_large'] == 'Other genus']
+#%% PIE CHART: FREQUENCY OF 'OTHER SPECIES' TREE GENUS
+streetTreesOtherSpecies = streetTrees.loc[streetTrees['species_large'] == 'Other species']
 
-pieOtherGenusFreq = px.pie(streetTreesOthergenus, 
+pieOtherGenusFreq = px.pie(streetTreesOtherSpecies, 
     values = 'single_tree', 
-    names = 'genus', 
+    names = 'species', 
     title = '''Distribution of Tree Genus' within "Other Genus" category''')
 
 pieOtherGenusFreq.show()
-#%% BAR CHART OF TREE GENUS
-barGenusFreq = px.bar(streetTrees, 
-    x = 'genus', 
-    y = 'single_tree', 
-    title = 'Distribution of Tree Genus',
-    color = 'municipality')
+#%% 
 
-barGenusFreq.show()
+fig = px.bar(streetTrees, x = 'species_large', y = 'single_tree', color = 'municipality')
+fig.show()
 
-# %% CREATE BAR DIAGRAMS WITH BARS SORTED BY AMOUNT OF TREES
+
+# %%
